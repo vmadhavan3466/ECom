1e9af2c31c24cf07e81c89860e909dae9b49b363
vmadhavan3466m <vmadhavan3466@gmail.com>
2025-03-16 22:54:44 -0500

    added dynamic adding of images to products

--- store/views.py
+++ store/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-
+from .models import *
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products':products}
     return render(request, 'store/Store.html', context)
 
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        # ^NEeds to be handled later
+
+    context = {'items':items}
     return render(request, 'store/Cart.html', context)
 
 
