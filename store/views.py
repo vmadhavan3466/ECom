2ab281e54507b323a2eb6384512ab4c3af669b69
vmadhavan3466m <vmadhavan3466@gmail.com>
2025-03-17 13:50:05 -0500

    updated carts in checkout and cart page

--- store/views.py
+++ store/views.py
@@ -14,12 +14,22 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
         # ^NEeds to be handled later
 
-    context = {'items':items}
+    context = {'items':items, 'order':order}
     return render(request, 'store/Cart.html', context)
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        # ^NEeds to be handled later
+
+    context = {'items':items, 'order':order}
     return render(request, 'store/Checkout.html', context)
